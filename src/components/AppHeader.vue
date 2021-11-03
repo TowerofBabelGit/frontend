32fd5be093acf363eda9105920c78e88a507b89b
ArseniiOl <55720143+ArseniiOl@users.noreply.github.com>
2021-11-03 16:47:17 +0200

    layout

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,54 @@
+<template>
+  <header class="header">
+    <div class="header__img">
+      <img src="@/assets/img/sun.png" alt="">
+      <svg class="header__img-text" viewBox="0 0 70 70" width="60%">
+        <path d="M35,35m-23,0a23,23 0 1,1 46,0a23,23 0 1,1 -46,0" fill="none" id="tophalf"/>
+        <text>
+          <textPath xlink:href="#tophalf" startOffset="30%">Lorem ipsum</textPath>
+          <textPath xlink:href="#tophalf" startOffset="87%">Dolor sit</textPath>
+        </text>
+      </svg>
+    </div>
+
+    <nav class="header-nav">
+      <ul class="header-nav__list">
+        <li class="header-nav__item">
+          <a href="#">Lorem ipsum</a>
+        </li>
+        <li class="header-nav__item">
+          <a href="#">Dolor sit</a>
+        </li>
+      </ul>
+    </nav>
+
+    <div class="header__logo">
+      <img src="@/assets/img/svg/logo.svg" alt="">
+    </div>
+
+    <nav class="header-nav">
+      <ul class="header-nav__list">
+        <li class="header-nav__item">
+          <a href="#">Consectetur</a>
+        </li>
+        <li class="header-nav__item">
+          <a href="#">Adipiscing elit</a>
+        </li>
+      </ul>
+    </nav>
+
+    <button class="page-btn" type="button">
+      Connect wallet
+    </button>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader"
+}
+</script>
+
+<style scoped>
+
+</style>
