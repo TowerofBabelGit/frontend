cc3bdba88bee01ee1c653c76fb294dcabaeb416c
ArseniiOl <55720143+ArseniiOl@users.noreply.github.com>
2021-11-08 17:38:25 +0200

    add animation

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,12 +2,20 @@
   <header class="header">
     <div class="header__img">
       <img src="@/assets/img/sun.png" alt="">
-      <svg class="header__img-text" viewBox="0 0 70 70" width="60%">
+      <svg  class="header__img-text" viewBox="0 0 70 70" width="90%">
         <path d="M35,35m-23,0a23,23 0 1,1 46,0a23,23 0 1,1 -46,0" fill="none" id="tophalf"/>
         <text>
           <textPath xlink:href="#tophalf" startOffset="30%">Lorem ipsum</textPath>
           <textPath xlink:href="#tophalf" startOffset="87%">Dolor sit</textPath>
         </text>
+<!--        <defs>
+        <path d="M50,250c0-110.5,89.5-200,200-200s200,89.5,200,200s-89.5,200-200,200S50,360.5,50,250" id="textcircle">
+        </path>
+        </defs>
+        <text dy="70" textLength="1220">
+          &lt;!&ndash; textLength (essentially the circumference of the circle) is used as an alternative to letter-spacing for Firefox, which currently doesn't support letter-spacing for SVG &ndash;&gt;
+          <textPath xlink:href="#textcircle">Lorem ipsum Dolor sit</textPath>
+        </text>-->
       </svg>
     </div>
 
