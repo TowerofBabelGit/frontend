01dea1ad3fa6c63cbda8a0466113a313857eeb75
SaenkoVova <vova080520@gmail.com>
2021-11-30 11:43:27 +0200

    fix url

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -766,7 +766,7 @@
                   </div>
 
                   <a class="view-on"
-                     :href="`https://bscscan.com/address/${getAccount}`"
+                     :href="`https://bscscan.com/address/${item.owner}`"
                      target="_blank">
                     View on BscScan
                     <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
