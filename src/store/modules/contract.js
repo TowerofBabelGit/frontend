872e43e0bede9b12330ad01a425988d36ade7a89
Max <36541363+Maxim-Turovets@users.noreply.github.com>
2022-12-17 23:56:42 +0200

    Update contract.js

--- src/store/modules/contract.js
+++ src/store/modules/contract.js
@@ -4,7 +4,7 @@ export default {
     namespaced: true,
     state: {
         //contractAddress: '0x7DeeBC4FF5b7593cDa15af1B78fB63aA33bd5897',
-        contractAddress: '0x7DeeBC4FF5b7593cDa15af1B78fB63aA33bd5897',
+        contractAddress: '0xC2B7A02082BB1C8C523fE6ed0560b15eF99A1798',
         abi: abi,
         multiplier: 1e18,
     },
